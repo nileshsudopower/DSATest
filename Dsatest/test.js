69f24ad49f407936da15d6dcbd8f5c9aea934a47
nileshsudopower <164172569+nileshsudopower@users.noreply.github.com>
2024-05-12 10:22:36 +0530

    updates test

--- Dsatest/test.js
+++ Dsatest/test.js
@@ -1,8 +1,8 @@
-// // Q.Write a JavaScript for loop that iterates from 0 to 10 and prints only the even numbers.
+// // Q.1 Write a JavaScript for loop that iterates from 0 to 10 and prints only the even numbers.
 // for(i=0;i<=10;i+=2){
 //     console.log(i)
 // }
-// // Q.Write a JavaScript while loop that calculates the factorial of a given number.
+// // Q2. Write a JavaScript while loop that calculates the factorial of a given number.
 // var temp = 1
 // var number = 5
 // i=1
@@ -11,7 +11,7 @@
 //     i++
 // }
 // console.log(temp)
-// Q.Create a JavaScript function that takes an array of numbers as input and returns the sum of all the numbers using a for loop.
+// Q3.Create a JavaScript function that takes an array of numbers as input and returns the sum of all the numbers using a for loop.
 // numbers = [1,2,3,4,5]
 // function SumArray(numbers) {
 //     var temp1 = 0;
@@ -21,7 +21,9 @@
 //     console.log(temp1);
 //   }
 //   SumArray(numbers)
-// Q.Write a JavaScript program that generates a Fibonacci sequence up to a certain number n, using a while loop.
+
+// Q.4 Write a JavaScript program that generates a Fibonacci sequence up to a certain number n, using a while loop.
+
 // let num = 0;
 // let num1 = 1;
 // console.log(num)
@@ -34,7 +36,7 @@
 //   num1 = temp2;
 //   temp2 = num + num1;
 // }
-// Q. Create a JavaScript function that takes an array of strings as input and returns a new array with only the strings that have a length greater than 5 characters.
+// Q.5 Create a JavaScript function that takes an array of strings as input and returns a new array with only the strings that have a length greater than 5 characters.
 // function funarray(students){
 //     var tempvalue =[];
 //     for (var i = 0; i < students.length; i++) {
@@ -49,7 +51,7 @@
 // const newarray= funarray(students)
 // console.log(newarray)
 
-// Q. Create a JavaScript function that takes an array of strings as input and returns a new array with only the strings that have a length greater than 5 characters.
+// Q.6 Create a JavaScript function that takes an array of strings as input and returns a new array with only the strings that have a length greater than 5 characters.
 
 // const number = 7;
 // function isPrime(number) {
@@ -68,9 +70,57 @@
 
 //   console.log(`${number} is prime:`, isPrime(number));
 
-// Q .Create a JavaScript program that calculates the sum of all numbers from 1 to 1000 that are divisible by both 3 and 5.
-for (let i = 1; i <= 1000; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log(i);
-  }
+// Q7 .Create a JavaScript program that calculates the sum of all numbers from 1 to 1000 that are divisible by both 3 and 5.
+// for (let i = 1; i <= 1000; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(i);
+//   }
+// }
+// Q8 .Write a JavaScript function that takes a string as input and returns the reverse of the string using a for loop.
+// function reversestring(string) {
+//   var temp4 = "";
+//   for (var i = string.length - 1; i >= 0; i--) {
+//     temp4 += string[i];
+//   }
+//   console.log(temp4);
+// }
+
+// reversestring("nilesh")
+
+// Q9. Create a JavaScript program that generates a random password of a specified length, consisting of letters (both uppercase and lowercase) and numbers.
+
+function randomPass() {
+    let password = '';
+    let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789';
+ 
+    for (let i = 1; i <= 8; i++) {
+        let char = Math.floor(Math.random()
+            * string.length + 1);
+ 
+        password += string.charAt(char)
+    }
+ 
+    return password;
 }
+ 
+console.log(randomPass());
+
+// Q10 .Write a JavaScript function that takes an array of numbers as input and returns the maximum difference between any two numbers in the array.
+
+// const numbers1=[1,2,3,4,5,6,7,8,9,10]
+// let min=numbers1[0];
+// let max=numbers1[9];
+// for(let i=0;i<numbers1.length;i++){
+//     if(numbers1[i]<min){
+//         min=numbers1[i]; 
+//     }
+
+//     if(numbers1[i]>max){
+//         max=numbers1[i]; 
+//     }
+// }
+// console.log(min)
+// console.log(max)
+// const new1 = max - min
+// console.log(new1)
